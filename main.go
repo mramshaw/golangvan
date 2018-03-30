63bb92f269d0e6e20b19f6f5a6b37f368b687204
mramshaw <mramshaw@alumni.concordia.ca>
2018-03-30 09:08:06 -0700

    Add notes on format verbs for runes

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ func main() {
 	str := "Hello world!"
 
 	for _, c := range str {
-		fmt.Printf("%[01]s %[01]v %[01]T\n", c)
+
+        // Technically, c is a rune, so 'go tool vet' will complain
+        // So lets use appropriate format verbs for a rune
+        // Specifically hexadecimal (%x) and Unicode (%U)
+
+		//fmt.Printf("%[01]s %[01]v %[01]T\n", c)
+		fmt.Printf("%[01]x %[01]U %[01]v %[01]T\n", c)
 	}
 }
